7e4dda120c8d3aa2494be1abf15d2c6172025e0b
Francesco Fraternali <francesco.fraternali@gmail.com>
2019-12-05 17:57:24 -0800

    Update batt_conversion.py

--- batt_conversion.py
+++ batt_conversion.py
@@ -6,11 +6,20 @@ def main():
 	raw_temp_data = sys.argv[1]	# Start with raw data from SensorTag
 	raw_temp_bytes = raw_temp_data.split() # Split into individual bytes
 	batt_percent = int(raw_temp_bytes[2], 16)
-	performance = int(raw_temp_bytes[1], 16)
-        reed = int(raw_temp_bytes[0], 16)
-	PIR_Out = int(raw_temp_bytes[5], 16)
-	light_2 = int(raw_temp_bytes[3], 16)
-	light_1 = int(raw_temp_bytes[4], 16)
+	PIR_reed = int(raw_temp_bytes[3], 16)
+	if PIR_reed == 0:
+		PIR_Out = 0; reed = 0
+	elif PIR_reed == 1:
+		PIR_Out = 1; reed = 0
+	elif PIR_reed == 2:
+		PIR_Out = 0; reed = 1
+	elif PIR_reed == 3:
+		PIR_Out = 1, reed = 1
+	
+        #reed = int(raw_temp_bytes[0], 16)
+	#PIR_Out = int(raw_temp_bytes[5], 16)
+	light_2 = int(raw_temp_bytes[0], 16)
+	light_1 = int(raw_temp_bytes[1], 16)
 	light = (light_1 << 8) | (light_2);
 	#light = light*4  # if you use an external case
 	light = light
