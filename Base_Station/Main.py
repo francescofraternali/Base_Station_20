797ad7e2c9dc6ec96c78f31a5a18ec1dd2603e07
Francesco Fraternali <frfrater@eng.ucsd.edu>
2020-03-14 14:19:49 -0700

    updates

--- Base_Station/Main.py
+++ Base_Station/Main.py
@@ -19,8 +19,8 @@ with open("../ID/ID.txt", "r") as f:
     for line in f:
         splitted = line.split(',')
         print(splitted)
-        Name_spl.append(splitted[0]) 
-        File_spl.append(splitted[1]) 
+        Name_spl.append(splitted[0])
+        File_spl.append(splitted[1])
 #print(Name_spl, File_spl)
 
 if len(Name_spl) == len(File_spl):
@@ -55,8 +55,8 @@ for i in range(len(File_spl)):
 #quit()
 
 #finder_time = 3 # time needed to avoid that multiple process are called and not completly killed. Put 3 for one sensor and 1 for several sensors
-write_completed = 1 #after you write a data you avoid to call the sensor again. 
-tryals = 5 #number of trials it looks for a specific device. Each try is 0.5s. 
+write_completed = 1 #after you write a data you avoid to call the sensor again.
+tryals = 5 #number of trials it looks for a specific device. Each try is 0.5s.
 
 def kill_search():
 	subprocess.Popen("killall Find_New_BLE_Device.sh 2>/dev/null", shell=True)
@@ -80,7 +80,7 @@ def check():
                 #print('here')
                 with open('wait.txt', 'r') as f:
                     first_line = f.readline()
-			
+
                 first = first_line[:1]
                 if first == '2':
                     sleep(write_completed)
@@ -92,7 +92,7 @@ def get_raw_data(ID):
     with open('wait.txt', 'w') as f:
         f.write('0')
         sleep(0.1)
-                    
+
     for i in range(len(ID_List)):
         if ID == ID_List[i]:
             Name = Name_List[i]
@@ -108,21 +108,21 @@ def get_raw_data(ID):
                 #print(splt[0], splt[2])
                 Action = splt[2]
                 break
-        
+
         if Action == '-1':
             print("Pay Attention: Name and Action not found")
             Action = '0'
 
-    #print("bash get_data_from_device.sh "+Name+" "+ID+" "+File+" "+Action) 
+    #print("bash get_data_from_device.sh "+Name+" "+ID+" "+File+" "+Action)
     subprocess.Popen("bash get_data_from_device.sh "+Name+" "+ID+" "+File+" "+Action+" &", shell=True)
-   
+
     #subprocess.Popen('bash get_data_from_device.sh ' +Name+' '+ID+' '+File+' '+Action+' &', shell=True)
     #print('checking')
     check()
     #print('checking over')
     killer()
     sleep(0.5)
-                    
+
 def get_action_name(ID):
     for i in range(len(ID_List)):
         if ID == ID_List[i]:
@@ -140,22 +140,22 @@ def get_action_name(ID):
                 Action = splt[2]
                 break
     return (Action, Name, File)
-     
+
 
 def check_reboot():
     proc = subprocess.Popen("cat /var/log/auth.log | grep 'Accepted password' > Accepted_file.txt", stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
-	
+
     for line in reversed(open('Accepted_file.txt').readlines()):
         #print(line.rstrip())
         spl = line.rstrip()
-	spl = spl.split(' ')
-	if 'Accepted' in spl and spl[1].isdigit():
-	    break
-	
+        spl = spl.split(' ')
+        if 'Accepted' in spl and spl[1].isdigit():
+            break
+
     print(spl)
     clock = spl[2].split(':')
-	
+
     now = datetime.datetime.now()
     now_time = now.strftime('%m/%d/%y %H:%M:%S')
 
@@ -167,29 +167,29 @@ def check_reboot():
         last_reset = datetime.datetime.strptime(out[0], '%m/%d/%y %H:%M:%S')
         #print("file exists", now_last)
     else:
-    	with open('last_reset.txt', 'w') as f:
+        with open('last_reset.txt', 'w') as f:
     	    f.write(now_time)
-	print("File does not exist. Creating it", now)
-    	last_reset = now
-	
+        print("File does not exist. Creating it", now)
+        last_reset = now
+
     diff_1 = (now - last_bs_read_time).total_seconds()
     diff_2 = (now - last_reset).total_seconds()
-    
+
     print(str(int(diff_1)) + "/3600, " + str(int(diff_2)) + "/14400")
     if diff_1 > 60*60*1:  # if nobody is reading into the BS for diff_1 time then I put all actions to 0 to avoid node dying
-	action_imposed = 0
-	print("action imposed to 0 since bs in not reading data")
-	if diff_2 > 60*60*4:  # if nobody is writing for diff_1 time and the BS was one for one day, then reboot
-	    with open('last_reset.txt', 'w') as f:
-	        f.write(now_time)
-	    sleep(1)
-            print("Nobody wants me. Or maybe I am broken? Reeboting...")
-            subprocess.Popen("sudo reboot", shell=True)
+        action_imposed = 0
+        print("action imposed to 0 since bs in not reading data")
+    if diff_2 > 60*60*4:  # if nobody is writing for diff_1 time and the BS was one for one day, then reboot
+        with open('last_reset.txt', 'w') as f:
+            f.write(now_time)
+        sleep(1)
+        print("Nobody wants me. Or maybe I am broken? Reeboting...")
+        subprocess.Popen("sudo reboot", shell=True)
     else:
-	action_imposed = -1
+        action_imposed = -1
 
     return action_imposed
-	
+
 print("Let us Start!!")
 
 #Reset BLE drivers
@@ -202,7 +202,7 @@ count_empty = 0
 action_imposed = -1
 
 while(True):
-    #print('here')	
+    #print('here')
     Name = ''
     File = ''
     #print("scanning")
@@ -216,19 +216,19 @@ while(True):
     found = []
     if (os.stat('dev_found.txt').st_size < 2) or (os.stat('ble_err.txt').st_size > 1):
         print('dev_found empty or blescan error')
-	print('blescan stderr file dimension:', os.stat('ble_err.txt').st_size)
+        print('blescan stderr file dimension:', os.stat('ble_err.txt').st_size)
         sleep(5)
-	if count_empty >= 10:
-	    print('List Empty. No devices found for a while. Rebooting BS.')
-	    subprocess.Popen('sudo reboot', shell=True)
+        if count_empty >= 10:
+            print('List Empty. No devices found for a while. Rebooting BS.')
+            subprocess.Popen('sudo reboot', shell=True)
             sleep(5)
         else:
-	    print('List Empty. No devices found, something wrong? Resetting BLE hci0 adapter')
-	    subprocess.Popen('sudo hciconfig hci0 reset', shell=True)
-	    count_empty += 1
-	
+            print('List Empty. No devices found, something wrong? Resetting BLE hci0 adapter')
+            subprocess.Popen('sudo hciconfig hci0 reset', shell=True)
+            count_empty += 1
+
     else:
-	count_empty = 0
+        count_empty = 0
         with open("dev_found.txt", 'r') as f:
             for line in f:
                 line = line.strip()
@@ -244,12 +244,12 @@ while(True):
                         Action, Name, File = get_action_name(ID)
                         log_temp = File.split('/')
                         log = log_temp[-1]
-                    
+
                         t = time.strftime('%m/%d/%y %H:%M:%S')
-			
-			if action_imposed == 0:
-			    Action = '0'
- 		
+
+                        if action_imposed == 0:
+                            Action = '0'
+
                         subprocess.Popen("bash Detector.sh " + Name + " " + ID + " " + File + " " + Action + " " + log + " 2>error.txt &", shell=True)
 
 
@@ -257,7 +257,7 @@ while(True):
     if len(avoid) > 0:
         #print('remove avoiding last ID')
         for ID in ID_List:
-            if ID in avoid: 
+            if ID in avoid:
                 avoid.remove(ID)
                 #print('removing ', ID)
 
@@ -268,8 +268,8 @@ while(True):
             avoid.append(ID)
             #avoid.append(ID)
             #print('avoiding ', ID)
-        
-       
+
+
     #if len(found) > 0:
     #    #for i in range(len(ID_List)):
     #    avoid.append(found[-1])
@@ -294,14 +294,14 @@ while(True):
     sleep(1)
     countarell += 1
     if countarell >= 360: # Use 360 as default that is 60*30/5 sec
-	try:
-	    print("checking reboot")
-    	    action_imposed = check_reboot()
-	except Exception as e: print("something wrong in check_reboot with error: ", e)
-	
-	countarell = 0
+        try:
+            print("checking reboot")
+            action_imposed = check_reboot()
+        except Exception as e: print("something wrong in check_reboot with error: ", e)
+
+        countarell = 0
         sleep(1)
-	
+
 print("It's Over")
 
 #sudo hciconfig hci0 reset
